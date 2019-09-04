97e10edfdd01bce92b0b5e5346d48122a666a0b3
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-04 22:06:30 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,94 @@
+def reverse_each_word (string)
+  array = [string.split]
+  reversed_array = []
+  final_array = []
+  counter = 0 
+  while counter < array.length 
+  reversed_array << array[counter].each do string.reverse
+end
+  counter +=1 
+end
+  
+# reversed_array.join("")
+end
+#  new_string = ""
+#  array.each do |word|
+#    new_string << "#{word.reverse}
+#  end
+#  new_string.join("")
+#end
+
+
+#  reversed_array = []
+#  final_array = []
+#  counter = 0 
+#  while counter < array.length 
+#  reversed_array << string.reverse
+#  counter +=1 
+#end
+#reversed_array.reverse.join("")
+#return reversed_array.join("")
+
+#end
+  
+#  array = [string.split("")]
+  
+#  reversed_array = []
+#  reversed_array << array.each do  
+#    reversed_array[string].reverse
+#  end
+#    string.reverse
+#    end
+#return reversed_array.join("")
+#end
+
+
+#  new_array = []
+#  new_array << array.each do |letters|
+#    letters.split
+#  end
+#  new_array.join
+# end 
+
+
+
+
+
+#def reverse_each_word (string)
+#  array = [string.split]
+#  new_string = ""
+#  array.collect do |flip|
+#    new_string << "#{flip.reverse}"
+#  end
+#  new_string.rstrip
+#end
+
+
+
+
+
+
+
+#  new_array = []
+#  final_array = []
+  
+#  while counter < array.length
+#  array[counter].each do |flip|
+#    new_array << array.split
+#    counter +=1
+#  end
+  
+#yield(new_array)
+#  new_array  
+#end
+
+
+
+  #set up counter to move through each element in order, reverse each element, and put in new array
+  #counter = 0 
+  #new_array = []
+  #while counter < array.length
+  #  new_array << (array[counter])
+  #  words = array[counter].reverse
+#    each do |letters| array[counter].reverse
+#    counter += 1
