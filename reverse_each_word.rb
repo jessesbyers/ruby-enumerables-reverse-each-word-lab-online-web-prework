59d672488db422ac20f80b73d8085712a188bc85
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-04 22:46:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
-def reverse_each_word (string)
-  array = string.split
-  reversed_array = []
-  counter = 0 
-  while counter < array.length do
-  reversed_array << array[counter].reverse
-  counter +=1 
-end
-reversed_array.join(' ')
-end
+#def reverse_each_word (string)
+#  array = string.split
+#  reversed_array = []
+#  counter = 0 
+#  while counter < array.length do
+#  reversed_array << array[counter].reverse
+#  counter +=1 
+#end
+#reversed_array.join(' ')
+#end
 
-def uses_collect (string)
+def reverse_each_word (string)
   array = string.split
   reversed_array = []
   array.collect do |flip|
