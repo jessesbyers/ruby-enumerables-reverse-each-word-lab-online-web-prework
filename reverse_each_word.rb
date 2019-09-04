b5129e12c5c0ac21ce62751ec75f986adc847312
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-04 23:30:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,8 +11,7 @@
 
 def reverse_each_word (string)
   array = string.split
-  reversed_array = array.collect {|word| word.reverse}
-reversed_array.join(' ')
+  reversed_array = array.collect {|word| word.reverse}.join(' ')
 end
 
 
